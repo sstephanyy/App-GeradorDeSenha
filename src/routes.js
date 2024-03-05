6b93476b5f29d5234a625c601fe6cbb9937481c0
Stephanyy <Stephanyy@DESKTOP-KVJF7PV>
2024-03-05 16:42:32 -0300

    customizing tab bar

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from './pages/home'
 import { Passwords } from "./pages/passwords";
+import { Ionicons } from "@expo/vector-icons"
 
 const Tab = createBottomTabNavigator();
 
@@ -10,11 +11,29 @@ export function Routes(){
             <Tab.Screen
                 name="Home"
                 component={Home}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({focused, size, color}) => {
+                        if(focused){ //focused means if a particular tab is currently active or selected.
+                            return <Ionicons size={size} color={"#392de9"} name="home"/>
+                        }
+                        return <Ionicons size={size} color={"#392de9"} name="home-outline"/>
+                    }
+                }}
             />
 
             <Tab.Screen
                 name="Senhas"
                 component={Passwords}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({focused, size, color}) => {
+                        if(focused){ 
+                            return <Ionicons size={size} color={"#392de9"} name="lock-closed"/>
+                        }
+                        return <Ionicons size={size} color={"#392de9"} name="lock-open"/>
+                    }
+                }}
             />
 
         </Tab.Navigator>
